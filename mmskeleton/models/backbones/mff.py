f264b8548c30af864d2b1c6f5d365752dea6d9ff
sj-li <lishijie1234@outlook.com>
2019-09-19 16:24:09 +0200

    finish GCNModule

--- mmskeleton/models/backbones/mff.py
+++ mmskeleton/models/backbones/mff.py
@@ -167,19 +167,96 @@ class MFFModule(nn.Module):
         return feat
 
         
+# class GCNModule(nn.Module):
+#     def __init__(self,
+#                  in_channels,
+#                  out_channels,
+#                  kernel_size,
+#                  stride=1,
+#                  bias = True,
+#                  residual = True):
+
+#         super().__init__()
+        
+#         self.kernel_size = kernel_size
+#         self.conv = nn.Conv2d(in_channels,
+#                               out_channels * kernel_size,
+#                               kernel_size = (kernel_size, 1),
+#                               bias=bias)
+
+#         if not residual:
+#             self.residual = lambda x: 0
+
+#         elif (in_channels == out_channels) and (stride == 1):
+#             self.residual = lambda x: x
+
+#         else:
+#             self.residual = nn.Sequential(
+#                 nn.Conv2d(in_channels,
+#                           out_channels,
+#                           kernel_size=1,
+#                           stride=(stride, 1)),
+#                 nn.BatchNorm2d(out_channels),
+#             )
+    
+#     def forward(self, x, A):
+#         assert A.size(0) == self.kernel_size
+
+#         res = self.residual(x)
+
+#         print(x.shape)
+#         x = self.conv(x)
+#         print(x.shape)
+
+#         # n, kc, t, v = x.size()
+#         # x = x.view(n, self.kernel_size, kc // self.kernel_size, t, v)
+#         # x = torch.einsum('nkctv,kvw->nctw', (x, A))
+#         # x = x.contiguous()
+
+#         # x = x + res
+
+#         return x, A
+
 class GCNModule(nn.Module):
+    r"""Applies a spatial temporal graph convolution over an input graph sequence.
+
+    Args:
+        in_channels (int): Number of channels in the input sequence data
+        out_channels (int): Number of channels produced by the convolution
+        kernel_size (tuple): Size of the temporal convolving kernel and graph convolving kernel
+        stride (int, optional): Stride of the temporal convolution. Default: 1
+        dropout (int, optional): Dropout rate of the final output. Default: 0
+        residual (bool, optional): If ``True``, applies a residual mechanism. Default: ``True``
+
+    Shape:
+        - Input[0]: Input graph sequence in :math:`(N, in_channels, T_{in}, V)` format
+        - Input[1]: Input graph adjacency matrix in :math:`(K, V, V)` format
+        - Output[0]: Outpu graph sequence in :math:`(N, out_channels, T_{out}, V)` format
+        - Output[1]: Graph adjacency matrix for output data in :math:`(K, V, V)` format
+
+        where
+            :math:`N` is a batch size,
+            :math:`K` is the spatial kernel size, as :math:`K == kernel_size[1]`,
+            :math:`T_{in}/T_{out}` is a length of input/output sequence,
+            :math:`V` is the number of graph nodes.
+
+    """
     def __init__(self,
                  in_channels,
                  out_channels,
                  kernel_size,
                  stride=1,
-                 bias = True,
-                 residual = True):
-        
+                 dropout=0,
+                 residual=True):
+        super().__init__()
+
+
         self.kernel_size = kernel_size
         self.conv = nn.Conv2d(in_channels,
                               out_channels * kernel_size,
-                              bias=bias)
+                              kernel_size=1,
+                              bias=True)
+
 
         if not residual:
             self.residual = lambda x: 0
@@ -195,19 +272,19 @@ class GCNModule(nn.Module):
                           stride=(stride, 1)),
                 nn.BatchNorm2d(out_channels),
             )
-    
+
+        self.relu = nn.ReLU(inplace=True)
+
     def forward(self, x, A):
         assert A.size(0) == self.kernel_size
 
         res = self.residual(x)
 
         x = self.conv(x)
-
         n, kc, t, v = x.size()
         x = x.view(n, self.kernel_size, kc // self.kernel_size, t, v)
-        x = torch.einsum('nkctv,kvw->nctw', (x, A))
-        x = x.contiguous()
-
+        x = torch.einsum('nkctv,kvw->nctw', (x, A)).contiguous()
+        
         x = x + res
 
-        return x, A
+        return self.relu(x), A
