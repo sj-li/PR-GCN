2e4ada8d315212b88648a3985ee1d2b7b5b5fde1
sj-li <lishijie1234@outlook.com>
2019-10-08 17:11:04 +0200

    sync

--- mmskeleton/models/backbones/mff8.py
+++ mmskeleton/models/backbones/mff8.py
@@ -41,68 +41,55 @@ class MFFNet8(nn.Module):
         self.register_buffer('A', A)
 
         # build networks
-        spatial_kernel_size = A.size(0)
-        temporal_kernel_size = 9
-        kernel_size = (temporal_kernel_size, spatial_kernel_size)
         self.data_bn = nn.BatchNorm1d(in_channels * A.size(1))
         kwargs0 = {k: v for k, v in kwargs.items() if k != 'dropout'}
 
         self.conv_shift_1 = nn.Conv2d(3, 64, 1)
-        self.gcn_shift_1 = gcn(64, 128, 3)
-        self.gcn_shift_2= gcn(128, 128, 3)
+        self.gcn_shift_1 = gcn_o(64, 128, 3)
+        self.gcn_shift_2= gcn_o(128, 128, 3)
         self.conv_shift_2 = nn.Conv2d(128, 3, 1)
 
         self.A_shift_1 = nn.Parameter(A + 0.0001*torch.ones(A.size()))
         self.A_shift_2 = nn.Parameter(A + 0.0001*torch.ones(A.size()))
 
-        self.tcn_motion_in = tcn(in_channels, 64, 3, 1, 1)
-        self.tcn_pos_in_1 = tcn(in_channels, 64, 3, 1, 1)
-        self.tcn_pos_in_2 = tcn(in_channels, 64, 3, 1, 1)
+        self.tcn_motion_in = tcn(in_channels, 64, 3, 1, 1, residual=False)
+        self.tcn_pos_in_1 = tcn(in_channels, 64, 3, 1, 1, residual=False)
+        self.tcn_pos_in_2 = tcn(in_channels, 64, 3, 1, 1, residual=False)
         self.conv_fusion_in = nn.Conv2d(128, 64, 1)
 
         self.tcn = nn.ModuleList((
             tcn(64, 64, 3, 1, 1, residual=False),
             tcn(64, 64, 3, 2, 1, residual=False),
             tcn(64, 64, 3, 1, 1, residual=False),
-            # tcn(64, 64, 3, 1, 4, residual=False),
             tcn(64, 64, 3, 3, 1, residual=False)
         ))
 
         self.gcn = nn.ModuleList((
-            gcn(128, 64,  3),
-            gcn(128, 64,  3),
-            # gcn(128, 64,  3, residual=False),
-            gcn(128, 64,  3),
-            gcn(128, 256, 3)
+            gcn_o(128, 64, 3),
+            gcn_o(128, 64, 3),
+            gcn_o(128, 64, 3),
+            gcn_o(256, 256, 3)
         ))
-        self.A =  nn.ParameterList([
+
+        self.As =  nn.ParameterList([
                 nn.Parameter(A + 0.0001*torch.ones(A.size()))
                 for i in self.gcn
             ])
-
-        self.conv_trans = nn.Conv2d(256, 128, 1)
-        self.conv_gather = nn.Conv2d(256, 256, 1)
-
-        # self.tcn_ = nn.ModuleList((
-        #     tcn(64, 64, 3, 1, 1, residual=False),
-        #     tcn(64, 64, 3, 1, 1, residual=False),
-        #     tcn(64, 64, 3, 1, 1, residual=False),
-        #     tcn(64, 64, 3, 1, 1, residual=False),
-        #     tcn(256, 256, 3, 1, 1, residual=False)
-        # ))
 
         self.conv_fusion = nn.ModuleList((
             nn.Conv2d(128, 128, 1),
             nn.Conv2d(128, 128, 1),
             nn.Conv2d(128, 128, 1),
-            # nn.Conv2d(128, 128, 1),
-            nn.Conv2d(128, 128, 1)
+            nn.Conv2d(128, 256, 1)
         ))
 
-        # self.gcn_out = gcn(256, 256, 3)
-        # self.A_out =  nn.Parameter(A + 0.0001*torch.ones(A.size()))
+        self.gcn_gather = gcn(256, 256)
+        self.A_gather = nn.Parameter(A + 0.0001*torch.ones(A.size()))
 
-        self.soft = nn.Softmax(-2)
+
+        # self.conv_trans = nn.Conv2d(256, 128, 1)
+        # self.conv_gather = nn.Conv2d(256, 256, 1)
+        # self.soft = nn.Softmax(-2)
 
         # fcn for prediction
         self.fcn = nn.Conv2d(256, num_class, kernel_size=1)
@@ -144,7 +131,7 @@ class MFFNet8(nn.Module):
         feats = feats[::-1]
 
         x = x_
-        for gcn, conv_f, f, A_ in zip(self.gcn, self.conv_fusion, feats, self.A):
+        for gcn, conv_f, A_, f, in zip(self.gcn, self.conv_fusion, self.As, feats):
             if x.shape[2] > f.shape[2]:
                 x = self.scale_T(x, f.shape[2])
             if x.shape[2] < f.shape[2]:
@@ -152,14 +139,14 @@ class MFFNet8(nn.Module):
             x = conv_f(torch.cat([x, f], 1))
             x = gcn(x, A_)
 
-        # x = self.gcn_out(x, self.A_out)
+        x = self.gcn_gather(x, self.A_gather)
 
-        x_ = self.conv_trans(x)
-        x1 = x_.permute(0, 3, 1, 2).contiguous().view(N*M, V, -1)
-        x2 = x_.view(N*M, -1, V)
-        A = self.soft(torch.matmul(x1, x2) / x1.size(-1))
-        x = x.view(N*M, -1, V)
-        x = self.conv_gather(torch.matmul(x, A).view(N*M, 256, -1, V))
+        # x_ = self.conv_trans(x)
+        # x1 = x_.permute(0, 3, 1, 2).contiguous().view(N*M, V, -1)
+        # x2 = x_.view(N*M, -1, V)
+        # A = self.soft(torch.matmul(x1, x2) / x1.size(-1))
+        # x = x.view(N*M, -1, V)
+        # x = self.conv_gather(torch.matmul(x, A).view(N*M, 256, -1, V))
             
         # global pooling
         x = F.max_pool2d(x, x.size()[2:])
@@ -175,9 +162,6 @@ class MFFNet8(nn.Module):
         N, C, T, V = x.shape
         x = x.view(N, C, newT, T//newT, V)
         x, _ = torch.max(x, 3)
-        # print(type(x))
-        # print(x.shape)
-        # exit()
         x = x.squeeze()
 
         return x
@@ -228,7 +212,7 @@ class tcn(nn.Module):
 
         return self.relu(x)
 
-class gcn(nn.Module):
+class gcn_o(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, residual=True):
         super().__init__()
 
@@ -258,6 +242,78 @@ class gcn(nn.Module):
         x = self.bn(x) + res
 
         return self.relu(x)
+
+class gcn(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, residual=False, num_subset=3):
+        super().__init__()
+
+        # self.gcn = ConvTemporalGraphical(in_channels, out_channels, kernel_size)
+        self.bn = nn.BatchNorm2d(out_channels)
+
+        if not residual:
+            self.residual = lambda x: 0
+
+        elif (in_channels == out_channels) and (stride == 1):
+            self.residual = lambda x: x
+
+        else:
+            self.residual = nn.Sequential(
+                nn.Conv2d(in_channels,
+                          out_channels,
+                          kernel_size=1,
+                          stride=(stride, 1)),
+                nn.BatchNorm2d(out_channels),
+            )
+
+        self.relu = nn.ReLU(inplace=True)
+
+        self.num_subset = num_subset
+
+        # self.conv_trans_1 = nn.Conv2d(in_channels, 64, 1)
+        # self.conv_trans_2 = nn.Conv2d(in_channels, 64, 1)
+        # self.conv_gather = nn.Conv2d(in_channels, out_channels, 1)
+
+        inter_channels = out_channels//2
+        self.inter_c = inter_channels
+         
+        self.conv_a = nn.ModuleList()
+        # self.conv_b = nn.ModuleList()
+        self.conv_d = nn.ModuleList()
+        for i in range(self.num_subset):
+            self.conv_a.append(nn.Conv2d(in_channels, inter_channels, 1))
+            # self.conv_b.append(nn.Conv2d(in_channels, inter_channels, 1))
+            self.conv_d.append(nn.Conv2d(in_channels, out_channels, 1))
+
+        self.soft = nn.Softmax(-2)
+
+    def forward(self, x, A):
+        # res = self.residual(x)
+
+        # N, C, T, V = x.size()
+
+        # x1 = self.conv_trans_1(x).permute(0, 3, 1, 2).contiguous().view(N, V, -1)
+        # x2 = self.conv_trans_2(x).view(N, -1, V)
+        # PA = self.soft(torch.matmul(x1, x2) / x1.size(-1))
+        # x = x.view(N, -1, V)
+        # x = self.conv_gather(torch.matmul(x, PA).view(N, -1, T, V))
+        # x = self.bn(x) + res
+
+        # return self.relu(x)
+        N, C, T, V = x.size()
+        y = None
+        for i in range(self.num_subset):
+            m = self.conv_a[i](x)
+            A1 = m.permute(0, 3, 1, 2).contiguous().view(N, V, self.inter_c * T)
+            A2 = m.view(N, self.inter_c * T, V)
+            A1 = self.soft(torch.matmul(A1, A2) / A1.size(-1))  # N V V
+            A1 = A1 + A[i]
+            A2 = x.view(N, C * T, V)
+            z = self.conv_d[i](torch.matmul(A2, A1).view(N, C, T, V))
+            y = z + y if y is not None else z
+
+        # y = self.bn(y)
+        # y += self.residual(x)
+        return y
 
 
 class st_gcn_block(nn.Module):
