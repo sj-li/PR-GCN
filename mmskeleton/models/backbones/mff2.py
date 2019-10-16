20c0f26b4d1dc5f9555763c59fb6bdcc5ea49569
sj-li <lishijie1234@outlook.com>
2019-10-16 21:41:01 +0200

    mff2

--- mmskeleton/models/backbones/mff2.py
+++ mmskeleton/models/backbones/mff2.py
@@ -3,30 +3,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-import copy
-
 from mmskeleton.ops.st_gcn import ConvTemporalGraphical, Graph
 
 
+# gcn in self.gcn, remove [::-1]
 class MFFNet2(nn.Module):
-    r"""Spatial temporal graph convolutional networks.
-
-    Args:
-        in_channels (int): Number of channels in the input data
-        num_class (int): Number of classes for the classification task
-        graph_cfg (dict): The arguments for building the graph
-        edge_importance_weighting (bool): If ``True``, adds a learnable
-            importance weighting to the edges of the graph
-        **kwargs (optional): Other parameters for graph convolution units
-
-    Shape:
-        - Input: :math:`(N, in_channels, T_{in}, V_{in}, M_{in})`
-        - Output: :math:`(N, num_class)` where
-            :math:`N` is a batch size,
-            :math:`T_{in}` is a length of input sequence,
-            :math:`V_{in}` is the number of graph nodes,
-            :math:`M_{in}` is the number of instance in a frame.
-    """
     def __init__(self,
                  in_channels,
                  num_class,
@@ -34,7 +15,6 @@ class MFFNet2(nn.Module):
                  edge_importance_weighting=True,
                  **kwargs):
         super().__init__()
-
 
         # load graph
         self.graph = Graph(**graph_cfg)
@@ -44,91 +24,65 @@ class MFFNet2(nn.Module):
         self.register_buffer('A', A)
 
         # build networks
-        spatial_kernel_size = A.size(0)
-        temporal_kernel_size = 3
-        kernel_size = (temporal_kernel_size, spatial_kernel_size)
         self.data_bn = nn.BatchNorm1d(in_channels * A.size(1))
-        kwargs0 = {k: v for k, v in kwargs.items() if k != 'dropout'}
 
-        self.mov_layers = nn.ModuleList((
-                        nn.Conv2d(in_channels, 64, (3, 1), 1, (1, 0), 1),
-                        nn.Conv2d(64, 64,  (3, 1), 1, (2, 0), 2),
-                        nn.Conv2d(64, 64,  (3, 1), 1, (3, 0), 3)
-                         
+        self.conv_shift_1 = nn.Conv2d(3, 64, 1)
+        self.gcn_shift_1 = gcn_o(64, 64, 3, A)
+        self.tcn_shift = tcn(64, 128, 3, 1, 1)
+        self.gcn_shift_2= gcn_o(128, 128, 3, A)
+        self.conv_shift_2 = nn.Conv2d(128, 3, 1)
+
+        self.tcn_motion_in = tcn(in_channels, 64, 3, 1, 1)
+        self.tcn_pos_in_1 = tcn(in_channels, 64, 3, 1, 1)
+        self.tcn_pos_in_2 = tcn(in_channels, 64, 3, 1, 1)
+        self.conv_fusion_in = nn.Conv2d(128, 64, 1)
+
+        self.tcn = nn.ModuleList((
+            tcn(128, 64, 3, 1, 1),
+            tcn(128, 64, 3, 2, 1),
+	    gcn_o(128, 64, 3, A),
+            tcn(128, 64, 3, 1, 1),
+            tcn(128, 128, 3, 3, 1)
         ))
 
-        self.pos_layers = nn.ModuleList((
-                        st_gcn_block(in_channels,
-                         64,
-                         (1, spatial_kernel_size),
-                         1,
-                         1,
-                         residual=False,
-                         **kwargs0),
-                        st_gcn_block(64, 64, (1, spatial_kernel_size), 1, 2, **kwargs),
-                        st_gcn_block(64, 64, (1, spatial_kernel_size), 1, 4, **kwargs)
-                         
+        self.tcn_back = nn.ModuleList((
+            tcn(64, 64, 3, 1, 1),
+            tcn(64, 64, 3, 1, 1),
+            tcn(64, 64, 3, 2, 1),
+            tcn(64, 64, 3, 1, 1),
+            tcn(64, 64, 3, 1, 1)
         ))
 
-        self.fusion_layers = nn.ModuleList((
-                        st_gcn_block(128, 64,  (1, spatial_kernel_size), 1, 1, **kwargs),
-                        st_gcn_block(128, 64,  (1, spatial_kernel_size), 1, 1, **kwargs),
-                        st_gcn_block(128, 64,  (1, spatial_kernel_size), 1, 1, **kwargs),
-                         
+        self.gcn = nn.ModuleList((
+            gcn_o(64, 64, 3, A),
+            gcn_o(64, 64, 3, A),
+            gcn_o(64, 64, 3, A),
+            gcn_o(64, 64, 3, A),
+            gcn_o(64, 64, 3, A)
         ))
 
-        self.posses_layers = nn.ModuleList((
-            st_gcn_block(64, 128, kernel_size, 2, **kwargs),
-            st_gcn_block(128, 128, kernel_size, 1, **kwargs),
-            st_gcn_block(128, 256, kernel_size, 2, **kwargs)
-        ))
-
-        # initialize parameters for edge importance weighting
-        if edge_importance_weighting:
-            self.edge_importance_fusion = nn.ParameterList([
-                nn.Parameter(torch.ones(self.A.size()))
-                for i in self.fusion_layers
-            ])
-            self.edge_importance = nn.ParameterList([
-                nn.Parameter(torch.ones(self.A.size()))
-                for i in self.posses_layers
-            ])
-        else:
-            self.edge_importance_fusion = [1] * len(self.fusion_layers)
-            self.edge_importance= [1] * len(self.posses_layers)
+        self.tcn_end = tcn(128, 256, 9, 5, 1)
+        self.gcn_end = gcn(256, 256)
 
         # fcn for prediction
-        self.fusion = nn.Conv2d(448, 256, kernel_size=1)
         self.fcn = nn.Conv2d(256, num_class, kernel_size=1)
 
-    def normalize(self, feat):
-        N, C, T, V, M = feat.size()
-        feat = feat.permute(0, 4, 3, 1, 2).contiguous()
-        feat = feat.view(N * M, V * C, T)
-        feat = self.data_bn(feat)
-        feat = feat.view(N, M, V, C, T)
-        feat = feat.permute(0, 1, 3, 4, 2).contiguous()
-        feat = feat.view(N * M, C, T, V)
+    def shift_adjust(self, x):
+        shift = self.conv_shift_1(x)
+        shift = self.gcn_shift_1(shift)
+        shift = self.tcn_shift(shift)
+        shift = self.gcn_shift_2(shift)
+        shift = self.conv_shift_2(shift)
+        shift[:, 2, :, :] = 0
 
-        return feat
+        x = x + shift
+
+        return x
 
     def forward(self, x):
 
         # data normalization
         N, C, T, V, M = x.size()
-
-        # mov = copy.deepcopy(x)
-        # mov = mov.permute(0, 4, 3, 2, 1).contiguous()
-        # mov = mov.view(N*M, V, T, C)
-        # mov[:,:,:-1,:] = mov[:,:,1:,:] - mov[:,:,:-1,:]
-        # mov[:,:,-1,:] = mov[:,:,-2,:]
-        # mov = mov.permute(0, 1, 3, 2).contiguous()
-        # mov = mov.view(N * M, V * C, T)
-        # mov = self.data_bn(mov)
-        # mov = mov.view(N, M, V, C, T)
-        # mov = mov.permute(0, 1, 3, 4, 2).contiguous()
-        # mov = mov.view(N * M, C, T, V)
-
         x = x.permute(0, 4, 3, 1, 2).contiguous()
         x = x.view(N * M, V * C, T)
         x = self.data_bn(x)
@@ -136,27 +90,26 @@ class MFFNet2(nn.Module):
         x = x.permute(0, 1, 3, 4, 2).contiguous()
         x = x.view(N * M, C, T, V)
 
-        mov = torch.zeros_like(x)
-        mov.copy_(x)
-        mov = mov.permute(0, 3, 2, 1).contiguous() # (N*M, V, T, C)
-        mov[:,:,:-1,:] = mov[:,:,1:,:] - mov[:,:,:-1,:]
-        mov[:,:,-1,:] = mov[:,:,-2,:]
-        mov = mov.permute(0, 3, 2, 1).contiguous()
-        
+        x = self.shift_adjust(x)
 
+        motion = torch.zeros_like(x)
+        motion[:,:,1:,:] = x[:,:,1:,:] - x[:,:,:-1,:]
 
         # forwad
-        for l_pos, l_mov, l_fusion, importance in zip(self.pos_layers, self.mov_layers, self.fusion_layers, self.edge_importance_fusion):
-            x = l_pos(x, self.A * importance)
-            mov = l_mov(mov)
-            x = l_fusion(torch.cat([x, mov], 1), self.A * importance)
+        motion = self.tcn_motion_in(motion)
+        x_ = self.tcn_pos_in_1(x)
+        x = self.tcn_pos_in_2(x)
+        x = self.conv_fusion_in(torch.cat([x, motion], 1))
 
-        for l, importance in zip(self.posses_layers, self.edge_importance):
-            x = l(x, self.A * importance)
+        for gcn, tcn_b, tcn in zip(self.gcn, self.tcn_back, self.tcn):
+            x_ = tcn_b(gcn(x_))
+            x = tcn(torch.cat([x_, x], 1))
 
-        # global pooling
-        x = F.avg_pool2d(x, x.size()[2:])
-        x = x.view(N, M, -1, 1, 1).mean(dim=1)
+        x = self.tcn_end(x)
+        x = self.gcn_end(x)
+
+        x = F.max_pool2d(x, x.size()[2:])
+        x, _ = x.view(N, M, -1, 1, 1).max(dim=1)
 
         # prediction
         x = self.fcn(x)
@@ -164,81 +117,28 @@ class MFFNet2(nn.Module):
 
         return x
 
-    def extract_feature(self, x):
+    def scale_T(self, x, newT):
+        N, C, T, V = x.shape
+        x = x.view(N, C, newT, T//newT, V)
+        x, _ = torch.max(x, 3)
+        x = x.squeeze()
 
-        # data normalization
-        N, C, T, V, M = x.size()
-        x = x.permute(0, 4, 3, 1, 2).contiguous()
-        x = x.view(N * M, V * C, T)
-        x = self.data_bn(x)
-        x = x.view(N, M, V, C, T)
-        x = x.permute(0, 1, 3, 4, 2).contiguous()
-        x = x.view(N * M, C, T, V)
-
-        # forwad
-        for gcn, importance in zip(self.st_gcn_networks, self.edge_importance):
-            x = gcn(x, self.A * importance)
-
-        _, c, t, v = x.size()
-        feature = x.view(N, M, c, t, v).permute(0, 2, 3, 4, 1)
-
-        # prediction
-        x = self.fcn(x)
-        output = x.view(N, M, -1, t, v).permute(0, 2, 3, 4, 1)
-
-        return output, feature
+        return x
 
 
-class st_gcn_block(nn.Module):
-    r"""Applies a spatial temporal graph convolution over an input graph sequence.
-
-    Args:
-        in_channels (int): Number of channels in the input sequence data
-        out_channels (int): Number of channels produced by the convolution
-        kernel_size (tuple): Size of the temporal convolving kernel and graph convolving kernel
-        stride (int, optional): Stride of the temporal convolution. Default: 1
-        dropout (int, optional): Dropout rate of the final output. Default: 0
-        residual (bool, optional): If ``True``, applies a residual mechanism. Default: ``True``
-
-    Shape:
-        - Input[0]: Input graph sequence in :math:`(N, in_channels, T_{in}, V)` format
-        - Input[1]: Input graph adjacency matrix in :math:`(K, V, V)` format
-        - Output[0]: Outpu graph sequence in :math:`(N, out_channels, T_{out}, V)` format
-        - Output[1]: Graph adjacency matrix for output data in :math:`(K, V, V)` format
-
-        where
-            :math:`N` is a batch size,
-            :math:`K` is the spatial kernel size, as :math:`K == kernel_size[1]`,
-            :math:`T_{in}/T_{out}` is a length of input/output sequence,
-            :math:`V` is the number of graph nodes.
-
-    """
-    def __init__(self,
-                 in_channels,
-                 out_channels,
-                 kernel_size,
-                 stride=1,
-                 dilation=1,
-                 dropout=0,
-                 residual=True):
+class tcn(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, dilation=1, dropout=0, residual=False):
         super().__init__()
 
-        assert len(kernel_size) == 2
-        assert kernel_size[0] % 2 == 1
-        # padding = ((kernel_size[0] - 1) // 2, 0)
-        padding = ((dilation*(kernel_size[0]-1))//2, 0)
-        
-
-        self.gcn = ConvTemporalGraphical(in_channels, out_channels,
-                                         kernel_size[1])
+        padding = ((kernel_size - 1)*dilation // 2, 0)
 
         self.tcn = nn.Sequential(
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(in_channels),
+            # nn.ReLU(inplace=True),
             nn.Conv2d(
+                in_channels,
                 out_channels,
-                out_channels,
-                (kernel_size[0], 1),
+                (kernel_size, 1),
                 (stride, 1),
                 padding,
                 (dilation, 1)
@@ -264,10 +164,114 @@ class st_gcn_block(nn.Module):
 
         self.relu = nn.ReLU(inplace=True)
 
-    def forward(self, x, A):
-
+    def forward(self, x):
         res = self.residual(x)
-        x, A = self.gcn(x, A)
-        x = self.tcn(x) + res
+        x = self.tcn(x)
+        x = x + res
 
+        return self.relu(x)
+
+class gcn_o(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, A, stride=1, residual=True):
+        super().__init__()
+
+        self.A = nn.Parameter(A + 0.0001*torch.ones(A.size()))
+
+        self.gcn = ConvTemporalGraphical(in_channels, out_channels, kernel_size)
+        self.bn = nn.BatchNorm2d(out_channels)
+
+        if not residual:
+            self.residual = lambda x: 0
+
+        elif (in_channels == out_channels) and (stride == 1):
+            self.residual = lambda x: x
+
+        else:
+            self.residual = nn.Sequential(
+                nn.Conv2d(in_channels,
+                          out_channels,
+                          kernel_size=1,
+                          stride=(stride, 1)),
+                nn.BatchNorm2d(out_channels),
+            )
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        res = self.residual(x)
+        x, _ = self.gcn(x, self.A)
+        x = self.bn(x) + res
+
+        return self.relu(x)
+
+class gcn(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, residual=False, coff_embedding=2, num_subset=3):
+        super().__init__()
+
+
+        if not residual:
+            self.residual = lambda x: 0
+
+        elif (in_channels == out_channels) and (stride == 1):
+            self.residual = lambda x: x
+
+        else:
+            self.residual = nn.Sequential(
+                nn.Conv2d(in_channels,
+                          out_channels,
+                          kernel_size=1,
+                          stride=(stride, 1)),
+                nn.BatchNorm2d(out_channels),
+            )
+
+        self.num_subset = num_subset
+
+        inter_channels = out_channels//coff_embedding
+        self.inter_c = inter_channels
+
+        self.conv_a = nn.ModuleList()
+        self.conv_b = nn.ModuleList()
+        self.conv_d = nn.ModuleList()
+        for i in range(self.num_subset):
+            self.conv_a.append(nn.Conv2d(in_channels, inter_channels, 1))
+            self.conv_b.append(nn.Conv2d(in_channels, inter_channels, 1))
+            self.conv_d.append(nn.Conv2d(in_channels, out_channels, 1))
+
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.soft = nn.Softmax(-2)
+
+    def forward(self, x):
+        N, C, T, V = x.size()
+        y = None
+        for i in range(self.num_subset):
+            A1 = self.conv_a[i](x).permute(0, 3, 1, 2).contiguous().view(N, V, self.inter_c * T)
+            A2 = self.conv_b[i](x).view(N, self.inter_c * T, V)
+            A1 = self.soft(torch.matmul(A1, A2) / A1.size(-1))  # N V V
+            A1 = A1
+            A2 = x.view(N, C * T, V)
+            z = self.conv_d[i](torch.matmul(A2, A1).view(N, C, T, V))
+            y = z + y if y is not None else z
+
+        y = self.bn(y)
+        y += self.residual(x)
+        return y
+
+class gtcn(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, residual=True):
+        super(gtcn, self).__init__()
+        self.gcn = gcn(in_channels, out_channels)
+        self.tcn = tcn(out_channels, out_channels, kernel_size, stride)
+        self.relu = nn.ReLU()
+        if not residual:
+            self.residual = lambda x: 0
+
+        elif (in_channels == out_channels) and (stride == 1):
+            self.residual = lambda x: x
+
+        else:
+            self.residual = tcn(in_channels, out_channels, kernel_size, stride)
+
+    def forward(self, x, A):
+        x = self.tcn(self.gcn(x, A)) + self.residual(x)
         return self.relu(x)
