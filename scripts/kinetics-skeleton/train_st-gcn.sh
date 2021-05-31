c759c30a3186f9de152e138142ef962f4920fdd1
Shijie Li <lishijie1234@outlook.com>
2021-05-31 11:06:30 +0200

    Delete scripts directory

--- scripts/kinetics-skeleton/train_st-gcn.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#block(name=train_st-gcn, threads=2 memory=11000, gpus=2, hours=48)
-mmskl configs/recognition/st_gcn/kinetics-skeleton/train.yaml
--- scripts/xsub/train_cst_gcn_1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#block(name=cst_gcn_1_xsub, threads=4, memory=11000, gpus=2, hours=48)
-mmskl configs/recognition/cst_gcn/1/xsub/train.yaml
